3e7d64798acf8c4bb87d87900651d71248d52406
daerhiel <485120+daerhiel@users.noreply.github.com>
2025-04-26 17:49:30 +0200

    feature(Core): Add case insensitive map

---
+++ src/app/core/src/cache-map.ts
@@ -0,0 +1,21 @@
+function transform(key: string): string {
+  return key.toLowerCase();
+}
+
+export class CacheMap<T> extends Map<string, T> {
+  public override has(key: string): boolean {
+    return super.has(transform(key));
+  }
+
+  public override get(key: string): T | undefined {
+    return super.get(transform(key));
+  }
+
+  public override set(key: string, value: T): this {
+    return super.set(transform(key), value);
+  }
+
+  public override delete(key: string): boolean {
+    return super.delete(transform(key));
+  }
+}
