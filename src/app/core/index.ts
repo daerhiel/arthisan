d3e41cb0b86a4a20109f723ff064fec3739a154a
daerhiel <485120+daerhiel@users.noreply.github.com>
2025-05-06 23:11:43 +0200

    feature(Core): Add settings support

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -2,4 +2,5 @@ export * from './src/broadcast';
 export * from './src/api-handlers';
 export * from './src/object-map';
 export * from './src/utilities';
+export * from './src/settings';
 export * from './src/tables';
