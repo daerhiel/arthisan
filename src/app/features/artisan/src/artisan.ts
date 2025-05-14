28cf6f4ff48098d4787d7de2cc6bb5b2ecbec0f1
daerhiel <485120+daerhiel@users.noreply.github.com>
2025-05-14 12:09:47 +0200

    feature(Artisan): Add ingredient category support

--- src/app/features/artisan/src/artisan.ts
+++ src/app/features/artisan/src/artisan.ts
@@ -1,11 +1,15 @@
 import { computed, inject, Injectable } from '@angular/core';
 
-import { TableDefinition } from '@app/core';
-import { CraftingRecipeData, HouseItems, MasterItemDefinitions } from '@app/nw-data';
+import { ObjectMap, TableDefinition } from '@app/core';
+import { CraftingIngredientType } from '@app/nw-data';
 import { NwBuddy, NwI18n, NwIcon, NwPrice } from '@app/nw-buddy';
 import { GamingTools } from '@app/gaming-tools';
 import { Craftable, getIconInputs, getPriceInputs } from './craftable';
+import { Category } from './category';
 
+/**
+ * Represents the Artisan module that provides crafting functionality.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -13,6 +17,9 @@ export class Artisan {
   readonly i18n = inject(NwI18n);
   readonly data = inject(NwBuddy);
   readonly gaming = inject(GamingTools);
+
+  readonly #items = new ObjectMap<Craftable>();
+  readonly #categories = new ObjectMap<Category>();
 
   readonly craftables: TableDefinition<Craftable> = {
     name: 'recipes',
@@ -31,10 +38,56 @@ export class Artisan {
       for (const key of this.data.recipes.keys() ?? []) {
         const item = this.data.items.get(key);
         if (item && item.ItemClass.includes('Resource') && item.ItemClass.includes('Gem')) {
-          objects.push(new Craftable(this, key));
+          const craftable = this.getItem(key);
+          craftable && objects.push(craftable);
         }
       }
       return objects;
     })
   };
+
+  /**
+   * Gets a craftable item from cache; creates a new one if not found.
+   * @param id The ID of an item to retrieve.
+   * @returns The item if found or created; otherwise, null.
+   */
+  getItem(id: string): Craftable | null {
+    let item = this.#items.get(id) ?? null;
+    if (!item && (this.data.items.has(id) || this.data.housing.has(id))) {
+      item = new Craftable(this, id);
+      item && this.#items.set(id, item);
+    }
+    return item;
+  }
+
+  /**
+   * Gets a category from cache; creates a new one if not found.
+   * @param id The ID of a category to retrieve.
+   * @returns The category if found or created; otherwise, null.
+   */
+  getCategory(id: string): Category | null {
+    let category = this.#categories.get(id) ?? null;
+    if (!category && this.data.categories.has(id)) {
+      category = new Category(this, id);
+      category && this.#categories.set(id, category);
+    }
+    return category;
+  }
+
+  /**
+   * Gets a list of recipes that craft a specific item.
+   * @param id The ID of an ingredient to retrieve.
+   * @param type The type of an ingredient to get.
+   * @returns The ingredient if found; otherwise, null.
+   */
+  getIngredient(id: string, type: CraftingIngredientType): Craftable | Category | null {
+    switch (type) {
+      case 'Item':
+        return this.getItem(id);
+      case 'Category_Only':
+        return this.getCategory(id);
+      default:
+        throw new Error(`Ingredient type is not supported: ${type}`);
+    }
+  }
 }
--- src/app/features/artisan/src/ingredient.ts
+++ src/app/features/artisan/src/ingredient.ts
@@ -1,13 +1,22 @@
+import { computed, signal } from "@angular/core";
+
 import { CraftingIngredientType } from "@app/nw-data";
 import { Artisan } from "./artisan";
-// import { computed } from "@angular/core";
+import { Category } from "./category";
 
 export class Ingredient {
-  // readonly #item = computed(() => this._artisan.getIngredient(this.id, this.type));
+  readonly #item = computed(() => this._artisan.getIngredient(this.id, this.type));
+
+  readonly selected = signal<string | null>(null);
+  readonly item = computed(() => {
+    const item = this.#item();
+    if (item instanceof Category) {
+      const id = this.selected();
+      return item.items()?.find(item => item.id === id) ?? null;
+    }
+    return item;
+  });
 
   constructor(private readonly _artisan: Artisan, readonly id: string, readonly type: CraftingIngredientType, readonly quantity: number) {
-    // if (type === 'Category_Only') {
-    //   _artisan.
-    // }
   }
 }
